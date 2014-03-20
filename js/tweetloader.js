236d22188189c24a8ebd21dc22754a6897230680
Bouncer <dev@bouncer.nl>
2014-03-20 16:32:39 +0100

    Map update

--- js/tweetloader.js
+++ js/tweetloader.js
@@ -1,11 +1,11 @@
-var tweets = 5;
+var tweets = 15;
 var refresh = tweets * 2000;
 $(document).ready(function() {
 	$('#data').tweet({
 		modpath: 'http://vu.bouncer.nl/twitter/',
 		avatar_size: 32,
 		count: tweets,
-		query: "-sex -#sexdating -buttpug -cockring -milf -geil -geile -erotisch -erotische gratis lang:nl include:retweets",
+		query: "-sex -sexdating -sexdate -buttpug -cockring -milf -geil -geile -erotisch -erotische gratis lang:nl include:retweets",
 		refresh_interval: tweets * 2,
 		loading_text: "searching twitter..."
 	}).ready(function() {
@@ -34,6 +34,21 @@ function loadTweets() {
 			$(value).delay((refresh/(length))*(i)).show(1,function() {
 				$(value).hide().prependTo($('#query .tweet_list')).show('slow');
 				processText();
+				var location = $('#query .tweet_list li .location').html();
+				
+				if(location.length > 0) {
+					console.log(location);
+					// To add the marker to the map, use the 'map' property
+					geocoder.geocode( { 'address': location}, function(results, status) {
+						if (status == google.maps.GeocoderStatus.OK) {
+							map.panTo(results[0].geometry.location);
+							var marker = new google.maps.Marker({
+								map: map,
+								position: results[0].geometry.location
+							});
+						}
+					});
+				}
 			});
 		}
 	});
